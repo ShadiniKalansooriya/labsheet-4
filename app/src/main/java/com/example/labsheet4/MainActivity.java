a14d9bec1e5cc9dc602c3f0d6c822666acca1045
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-03-24 13:20:59 +0530

    commit 4

--- app/src/main/java/com/example/labsheet4/MainActivity.java
+++ app/src/main/java/com/example/labsheet4/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.labsheet4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = String.valueOf(0);
+    Button buttonSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSignup = (Button)findViewById(R.id.buttonSignUp);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+
+    }
+
+    public void signup() {
+        EditText editText = (EditText) findViewById(R.id.editTextName);
+        String name = editText.getText().toString();
+
+        Intent intent = new Intent(this, Notification.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Hello")
+                .setContentText("Hello "+name+"! welcome to the MAD team")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build());
+
+        Intent intent1 = new Intent(MainActivity.this, registration.class);
+        startActivity(intent1);
     }
 }
--- app/src/main/java/com/example/labsheet4/registration.java
+++ app/src/main/java/com/example/labsheet4/registration.java
@@ -2,7 +2,10 @@ package com.example.labsheet4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class registration extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        final Intent intent = new Intent(registration.this, MainActivity.class);
+        Button cancel_btn = (Button) findViewById(R.id.buttonCancel);
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
